6f561131f8100af482f3e98defcac463e318c491
Faisal Saeed <faisal@mariadb.com>
2021-09-02 04:13:01 +0800

    Fixed the typo

--- xonotic-sim.py
+++ xonotic-sim.py
@@ -81,7 +81,7 @@ def startGame(players):
     cursor = conn.cursor()
     totalPlayers = random.randrange(16, players)
 
-    stmtGame = "INSERT INTO game(game_name, total_players, end_time) VALUES(%s, %s)"
+    stmtGame = "INSERT INTO game(game_name, total_players) VALUES(%s, %s)"
     strValues = (strGenerator().title(), totalPlayers)
     cursor.execute(stmtGame, strValues)
     conn.commit()
