b85eac2490143a57650d3f631297f071f0e44853
Faisal Saeed <faisal@mariadb.com>
2021-09-03 03:09:59 +0800

    Scripts

--- dbscripts/Leaderboard.sql
+++ dbscripts/Leaderboard.sql
@@ -5,3 +5,9 @@ INNER JOIN gameplayer B on A.id = B.game_id
 INNER JOIN player P on B.player_id = P.id
 LEFT JOIN (select game_id, player_id, count(*) as deaths from leaderboard group by game_id, player_id) C on A.id = C.game_id AND B.player_id = C.player_id
 LEFT JOIN (select game_id, killed_by, count(*) as kills from leaderboard group by game_id, killed_by) D on A.id = D.game_id AND B.player_id = D.killed_by);
+
+
+CREATE VIEW v_top_leaderboard AS (
+SELECT ID as GameID, GameName, PlayerID, PlayerName, Kills, Deaths, A.RN 
+FROM (SELECT id, GameName, PlayerID, PlayerName, Kills, Deaths, ROW_NUMBER() OVER (PARTITION BY ID ORDER BY Kills DESC) AS RN from v_full_leaderboard) A
+);
