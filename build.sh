a2152513dbddfb192738d35a372019af0ebc4e27
Faisal Saeed <faisal@mariadb.com>
2021-09-05 00:12:32 +0800

    Build Script

---
+++ build.sh
@@ -0,0 +1,11 @@
+Version=$1
+if [ $? -ne 1 ]
+then
+    echo "Must specify the version number for the build and deployment"
+    echo "build.sh 1"
+    echo
+    exit 1
+fi
+docker build -t x-leaderboard:v${Version} .
+docker tag x-leaderboard:v${Version} gcr.io/$DEVSHELL_PROJECT_ID/x-leaderboard:v${Version}
+docker push gcr.io/$DEVSHELL_PROJECT_ID/x-leaderboard:v${Version}
