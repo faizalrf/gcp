84b1a96c20f9a6ffe570ce619d034b0ea96f12e2
Faisal Saeed <faisal@mariadb.com>
2021-09-05 02:34:56 +0800

    Updated with auto clean

--- app/build.sh
+++ app/build.sh
@@ -5,7 +5,12 @@ if [ "$#" -ne 1 ]; then
     echo
     exit 1
 fi
+# remove all local images
+docker images -a | grep "leaderboard" | awk '{print $3}' | xargs docker rmi
+
+# remove GCR images
 gcloud container images delete gcr.io/group1-6m11/x-leaderboard:v1
+
 kubectl delete -f ./deploy.yaml
 kubectl delete -f ./deploy-lb.yaml
 docker build -t x-leaderboard:v${Version} .
