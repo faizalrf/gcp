b5f78f991eab452c3c22482ff19fe7ffedc9633b
angliwen <angliwen.sg@gmail.com>
2021-09-05 05:55:54 +0000

    Fixed typo

--- patch-sample-ui-yaml/delete-ui.sh
+++ patch-sample-ui-yaml/delete-ui.sh
@@ -1,3 +1,3 @@
-kubectl delete -f ~/gcp/patch-sample-ui-yaml/deploy-ui.pool-ingress.yaml
-kubectl delete -f ~/gcp/patch-sample-ui-yaml/deploy-ui.pool-lb.yaml
-kubectl delete -f ~/gcp/patch-sample-ui-yaml/deploy-ui.pool.yaml
+kubectl delete -f ~/gcp/patch-sample-ui-yaml/deploy-ui-pool-ingress.yaml
+kubectl delete -f ~/gcp/patch-sample-ui-yaml/deploy-ui-pool-lb.yaml
+kubectl delete -f ~/gcp/patch-sample-ui-yaml/deploy-ui-pool.yaml
--- patch-sample-ui-yaml/deploy-ui.sh
+++ patch-sample-ui-yaml/deploy-ui.sh
@@ -1,3 +1,3 @@
-kubectl apply -f ~/gcp/patch-sample-ui-yaml/deploy-ui.pool.yaml
-kubectl apply -f ~/gcp/patch-sample-ui-yaml/deploy-ui.pool-lb.yaml
-kubectl apply -f ~/gcp/patch-sample-ui-yaml/deploy-ui.pool-ingress.yaml
+kubectl apply -f ~/gcp/patch-sample-ui-yaml/deploy-ui-pool.yaml
+kubectl apply -f ~/gcp/patch-sample-ui-yaml/deploy-ui-pool-lb.yaml
+kubectl apply -f ~/gcp/patch-sample-ui-yaml/deploy-ui-pool-ingress.yaml
