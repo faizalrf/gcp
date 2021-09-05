0bf9718adac77faa4bf3ce43f752a940c68942bc
Faisal Saeed <faisal@mariadb.com>
2021-09-06 00:49:30 +0800

    Fixed to V1

--- app/build.sh
+++ app/build.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 # remove all local images
-docker rmi $(docker images -a -q) -f
+# docker rmi $(docker images -a -q) -f
 
 # remove GCR images
 gcloud container images delete gcr.io/group1-6m11/x-leaderboard:v1
