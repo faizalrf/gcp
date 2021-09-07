3198f9a2a6d3672adc6326761f3063ce1a31b3b7
Faisal Saeed <faisal@mariadb.com>
2021-09-07 23:32:02 +0800

    Updated Timing

--- app/xonotic-sim.py
+++ app/xonotic-sim.py
@@ -156,7 +156,7 @@ def startGame(players):
 #Simulate random game play and kills
 def battleOn(conn, playerList, gameID):
     cursor = conn.cursor()
-    maxKills = random.randrange(500, 3500)
+    maxKills = random.randrange(500, 5000)
 
     for totalEvents in range(1, maxKills):
         # Get a random player ID as the RIP dude from the from the Player Dataframe
@@ -168,7 +168,7 @@ def battleOn(conn, playerList, gameID):
                         str(gameID) + ", " + str(randomPlayerID) + ", " + str(randomKillerID) + ", current_timestamp(6))"
 
         cursor.execute(stmtKill)
-        time.sleep(random.randrange(0, 2))
+        time.sleep(random.randrange(0, 1))
         print("Game Progress: ", str(round(totalEvents/maxKills*100))+"%", end="\r")
         conn.commit()
 
